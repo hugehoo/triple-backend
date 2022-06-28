07a450d713e18c2c2de3dfae124dfce628888617
hugehoo <tbnsok40@gmail.com>
2022-06-28 21:45:08 +0900

    Dto 적용

--- src/main/java/triple/mileage/service/factory/EventServiceImpl.java
+++ src/main/java/triple/mileage/service/factory/EventServiceImpl.java
@@ -3,7 +3,9 @@ package triple.mileage.service.factory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import triple.mileage.domain.Event;
+import triple.mileage.domain.Review;
 import triple.mileage.dto.EventDTO;
+import triple.mileage.dto.ReviewDto;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +13,9 @@ public class EventServiceImpl {
 
     private final EventFactory eventFactory;
 
-    public void getEvent(EventDTO.REVIEW reviewDto) {
+    public ReviewDto getEvent(EventDTO.REVIEW reviewDto) {
         Event event = Event.valueOf(reviewDto.getAction());
-        eventFactory.getEvent(event).apply(reviewDto);
+        Review review = eventFactory.getEvent(event).apply(reviewDto);
+        return ReviewDto.of(review);
     }
 }
